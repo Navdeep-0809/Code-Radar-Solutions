8fb5aef6029e2c1148a203d58dbf23cf139a6a39
Navdeep-0809 <surjeetsingh98498@gmail.com>
2025-02-14 12:29:46 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
-#include <limits.h> // To use INT_MAX
-
+#include <limits.h>
 int main() {
     int num;
-
-    // Ask the user to input an integer
-    printf("Enter an integer: ");
     scanf("%d", &num);
-
-    // Check the Most Significant Bit (MSB) for a 32-bit integer
     if (num & (1 << (sizeof(int) * CHAR_BIT - 1))) {
         printf("Set");
     } else {
         printf("Not Set");
     }
-
     return 0;
 }
