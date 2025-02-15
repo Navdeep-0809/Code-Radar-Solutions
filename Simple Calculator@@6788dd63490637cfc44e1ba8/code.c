e5c5f36dd19967007eb7f46b9c24210a7fb9dae1
Navdeep-0809 <surjeetsingh98498@gmail.com>
2025-02-15 12:20:48 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -15,10 +15,10 @@ int main() {
         if (num2 != 0) {
             printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
         } else {
-            printf("Error! Division by zero.\n");
+            printf("error");
         }
     } else {
-        printf("Error! Invalid operator.\n");
+        printf("error");
     }
 
     return 0;
